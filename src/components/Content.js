108111fe440f6c4660210153b2de12cd28cbe055
shakib <shakibn350@gmail.com>
2021-06-21 17:22:54 +0600

    Class component context api

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,21 +2,23 @@ import React from 'react';
 import HoverCounter from './HoverCounter';
 import themeContext from '../lib/themContext';
 
-const Content =()=>{
-    return (
-        <div>
-            <h1>I am Content function</h1>
-            <themeContext.Consumer >
-               {
-                   (value)=>{
-                       console.log("I am Content function ");
-                       console.log(value);
-                      return <HoverCounter theme={value.theme} changeTheme={value.changeTheme}/>
-                   }
-               }
-            </themeContext.Consumer>
-        </div>
-    )
-}
+class Content extends React.Component{
+    //Rule 1
+   //static contextType=themeContext
+   componentDidMount(){
+       console.log(this.context);
+   }
+   render(){
+       const {theme, changeTheme} =this.context;
 
+        return (
+            <div>
+                <h1>I am Content function</h1>                  
+                <HoverCounter theme={theme} changeTheme={changeTheme}/>
+            </div>
+        )
+   }
+}
+//Rule 2
+ Content.contextType=themeContext
 export default Content;

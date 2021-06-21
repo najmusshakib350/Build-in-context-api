ebc6f21950a6a6d39b9e78ce2ef9fcc170879659
shakib <shakibn350@gmail.com>
2021-06-21 17:57:34 +0600

    Functional-context-api

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,24 +1,21 @@
-import React from 'react';
+import React,{useEffect,useContext} from 'react';
 import HoverCounter from './HoverCounter';
 import themeContext from '../lib/themContext';
 
-class Content extends React.Component{
-    //Rule 1
-   //static contextType=themeContext
-   componentDidMount(){
-       console.log(this.context);
-   }
-   render(){
-       const {theme, changeTheme} =this.context;
+const Content =()=>{
+    const context =useContext(themeContext);
+    
+    useEffect(() =>{
+        console.log("I am useEffect function");
+        console.log(context);
+    });
 
-        return (
-            <div>
-                <h1>I am Content function</h1>                  
-                <HoverCounter theme={theme} changeTheme={changeTheme}/>
-            </div>
-        )
-   }
+    const {theme, changeTheme}=context;
+    return (
+        <div>
+            <h1>I am Content function</h1>                  
+            <HoverCounter theme={theme} changeTheme={changeTheme}/>
+        </div>
+    ) 
 }
-//Rule 2
- Content.contextType=themeContext
 export default Content;
